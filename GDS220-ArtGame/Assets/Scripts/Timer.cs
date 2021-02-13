1b4ef494f84b85c03b99ac8a8ae73097c3ec8aea
Tmitchell26 <61007681+Tmitchell26@users.noreply.github.com>
2021-02-14 00:18:07 +0800

    francine done, medic started

---
+++ GDS220-ArtGame/Assets/Scripts/Timer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    public Animator animator;
+
+    public float gameTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(startTimer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator startTimer()
+    {
+        gameTimer = 120f;
+
+        while (gameTimer > 0)
+        {
+            gameTimer -= Time.deltaTime;
+            yield return null;
+        }
+
+        if (gameTimer <= 0)
+        {
+            animator.SetBool("Crying", true);
+            yield return new WaitForSeconds(5f);
+            SceneManager.LoadScene("EndGame");
+        }
+    }
+}
