09d12c8a4d210513cffaab80a08644c6eba50504
Tmitchell26 <61007681+Tmitchell26@users.noreply.github.com>
2021-02-15 08:50:00 +0800

    extra assets and medic finished

--- GDS220-ArtGame/Assets/Scripts/Timer.cs
+++ GDS220-ArtGame/Assets/Scripts/Timer.cs
@@ -6,9 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
-    public Animator animator;
+    public Animator francine;
+    public Animator medic;
+    public Text gameTime;
 
     public float gameTimer;
+    public GameObject speech;
 
     // Start is called before the first frame update
     void Start()
@@ -16,26 +19,26 @@ public class Timer : MonoBehaviour
         StartCoroutine(startTimer());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     IEnumerator startTimer()
     {
-        gameTimer = 120f;
+        gameTimer = 120;
 
         while (gameTimer > 0)
         {
             gameTimer -= Time.deltaTime;
+            gameTime.text = gameTimer.ToString();
             yield return null;
         }
 
         if (gameTimer <= 0)
         {
-            animator.SetBool("Crying", true);
-            yield return new WaitForSeconds(5f);
+            medic.SetBool("Medic", true);
+            yield return new WaitForSeconds(3f);
+            speech.SetActive(true);
+            yield return new WaitForSeconds(3f);
+
+            francine.SetBool("Crying", true);
+            yield return new WaitForSeconds(6f);
             SceneManager.LoadScene("EndGame");
         }
     }
